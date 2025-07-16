b1d9d60333c86858fb0f95f3c6675ac3471c8d17
blamperer <137224671+blamperer@users.noreply.github.com>
2025-07-15 21:05:22 -0400

    oops compatibilities

--- modules/enemies/blacklizard.lua
+++ modules/enemies/blacklizard.lua
@@ -10,12 +10,23 @@ SMODS.Joker({
 	atlas = "enemies",
 	pos = { x = 7, y = 2 },
 	discovered = true,
-	blueprint_compat = true,
-	eternal_compat = true,
-	perishable_compat = true,
+	blueprint_compat = false,
+	perishable_compat = false,
+	rw_wbeehive_compat = false,
+	rw_wcherrybomb_compat = false,
+	rw_wspear_ele_compat = false,
+	rw_wspear_exp_compat = false,
+	rw_wspear_fire_compat = false,
+	rw_wflashbang_compat = false,
+	rw_wgrenade_compat = false,
+	rw_wjokerifle_compat = false,
+	rw_wrock_compat = false,
+	rw_wsingularity_compat = false,
+	rw_wspear_compat = false,
+	rw_wsporepuff_compat = false,
 	add_to_deck = function(self, card, from_debuff)
 		SMODS.Stickers["eternal"]:apply(card, true)
-        -- Threat
+		-- Threat
 		for _, v in pairs(G.playing_cards) do
 			if v:is_suit("Hearts", true) or v:is_suit("Diamonds", true) then
 				SMODS.debuff_card(v, true, "black_lizard_temporary")
@@ -32,13 +43,13 @@ SMODS.Joker({
 	calculate = function(self, card, context)
 		-- Defeat
 		if context.before and not context.blueprint then
-            if #context.poker_hands["Flush House"] > 0 then
-                local valid = true
-                for _,v in ipairs(context.poker_hands["Flush House"][1]) do
-                    valid = valid and not (v:is_suit("Hearts", true) or v:is_suit("Diamonds", true))
-                end
-                card.ability.extra.defeat = valid
-            end
+			if #context.poker_hands["Flush House"] > 0 then
+				local valid = true
+				for _, v in ipairs(context.poker_hands["Flush House"][1]) do
+					valid = valid and not (v:is_suit("Hearts", true) or v:is_suit("Diamonds", true))
+				end
+				card.ability.extra.defeat = valid
+			end
 		end
 		if context.after and card.ability.extra.defeat then
 			G.E_MANAGER:add_event(Event({
--- modules/enemies/eellizard.lua
+++ modules/enemies/eellizard.lua
@@ -11,9 +11,20 @@ SMODS.Joker({
 	atlas = "enemies",
 	pos = { x = 2, y = 2 },
 	discovered = true,
-	blueprint_compat = true,
-	eternal_compat = true,
-	perishable_compat = true,
+	blueprint_compat = false,
+	perishable_compat = false,
+	rw_wbeehive_compat = false,
+	rw_wcherrybomb_compat = false,
+	rw_wspear_ele_compat = false,
+	rw_wspear_exp_compat = false,
+	rw_wspear_fire_compat = false,
+	rw_wflashbang_compat = false,
+	rw_wgrenade_compat = false,
+	rw_wjokerifle_compat = false,
+	rw_wrock_compat = false,
+	rw_wsingularity_compat = false,
+	rw_wspear_compat = false,
+	rw_wsporepuff_compat = false,
 	loc_vars = function(self, info_queue, card)
 		return { vars = {
 			card.ability.extra.wetcount,
@@ -21,7 +32,7 @@ SMODS.Joker({
 	end,
 	add_to_deck = function(self, card, from_debuff)
 		SMODS.Stickers["eternal"]:apply(card, true)
-        -- Threat
+		-- Threat
 		for _, v in pairs(G.playing_cards) do
 			if v:is_suit("Spades", true) or v:is_suit("Clubs", true) then
 				SMODS.debuff_card(v, true, "eel_lizard_temporary")
