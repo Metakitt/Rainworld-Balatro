2684e01ff0244ddf0fb703f48cd4b32644cabc07
blamperer <137224671+blamperer@users.noreply.github.com>
2025-07-16 20:24:46 -0400

    fix random rank/suit selection

--- modules/foods/dandepeach.lua
+++ modules/foods/dandepeach.lua
@@ -9,7 +9,7 @@ SMODS.Consumable({
 	discovered = true,
 	config = { extra = { upgrade = 20 }, name = "dandepeach" },
 	loc_vars = function(self, info_queue, card)
-		return { vars = { card.ability.extra.upgrade }}
+		return { vars = { card.ability.extra.upgrade } }
 	end,
 	set_badges = function(self, card, badges)
 		badges[#badges + 1] = create_badge(localize("k_fooduncommon"), G.C.GREEN, G.C.WHITE, 1.2)
@@ -19,20 +19,14 @@ SMODS.Consumable({
 	end,
 	use = function(self, card, area, copier)
 		if card.ability.name == "dandepeach" then
-			_rank = pseudorandom_element(
-				{ "A", "J", "Q", "K", "2", "3", "4", "5", "6", "7", "8", "9", "T" },
-				pseudoseed("test")
-			)
-			_suit = pseudorandom_element({ "S", "H", "D", "C" }, pseudoseed("test"))
-			G.deck.config.card_limit = G.deck.config.card_limit + 1
-			local other_card = create_playing_card(
-				{ front = G.P_CARDS[_suit .. "_" .. _rank], center = G.P_CENTERS.c_base },
-				G.hand,
-				nil,
-				nil,
-				{ G.C.SECONDARY_SET.Enhanced }
-			)
-			playing_card_joker_effects({ other_card })
+			rank = pseudorandom_element(SMODS.Ranks, "rw_dandepeach_rank", {})
+			suit = pseudorandom_element(SMODS.Suits, "rw_dandepeach_suit", {})
+			local other_card = SMODS.add_card({
+				set = "Base",
+				rank = rank.key,
+				suit = suit.key,
+				area = G.hand,
+			})
 			other_card.ability.perma_bonus = other_card.ability.perma_bonus or 0
 			other_card.ability.perma_bonus = other_card.ability.perma_bonus + card.ability.extra.upgrade
 		end
--- modules/foods/popcornplant.lua
+++ modules/foods/popcornplant.lua
@@ -9,7 +9,7 @@ SMODS.Consumable({
 	discovered = true,
 	config = { extra = { upgrade = 2 }, name = "popcornplant" },
 	loc_vars = function(self, info_queue, card)
-		return { vars = { card.ability.extra.upgrade }}
+		return { vars = { card.ability.extra.upgrade } }
 	end,
 	set_badges = function(self, card, badges)
 		badges[#badges + 1] = create_badge(localize("k_fooduncommon"), G.C.GREEN, G.C.WHITE, 1.2)
@@ -20,60 +20,16 @@ SMODS.Consumable({
 	use = function(self, card, area, copier)
 		if card.ability.name == "popcornplant" then
 			local suit = pseudorandom_element(SMODS.Suits, pseudoseed("mysuit"))
-
-			if suit.key == "Clubs" then
-				for i = 1, #G.deck.cards do
-					local other_card = G.deck.cards[i]
-					if other_card:is_suit("Clubs") then
-						other_card.ability.perma_mult = other_card.ability.perma_mult or 0
-						other_card.ability.perma_mult = other_card.ability.perma_mult + card.ability.extra.upgrade
-						other_card.ability.perma_bonus = other_card.ability.perma_bonus or 0
-						other_card.ability.perma_bonus = other_card.ability.perma_bonus + card.ability.extra.upgrade
-						other_card:juice_up(0.5, 0.5)
-						SMODS.calculate_effect({ message = localize("k_upgrade_ex"), colour = G.C.CHIPS }, other_card)
-					end
-				end
-			end
-
-			if suit.key == "Hearts" then
-				for i = 1, #G.deck.cards do
-					local other_card = G.deck.cards[i]
-					if other_card:is_suit("Hearts") then
-						other_card.ability.perma_mult = other_card.ability.perma_mult or 0
-						other_card.ability.perma_mult = other_card.ability.perma_mult + card.ability.extra.upgrade
-						other_card.ability.perma_bonus = other_card.ability.perma_bonus or 0
-						other_card.ability.perma_bonus = other_card.ability.perma_bonus + card.ability.extra.upgrade
-						other_card:juice_up(0.5, 0.5)
-						SMODS.calculate_effect({ message = localize("k_upgrade_ex"), colour = G.C.CHIPS }, other_card)
-					end
-				end
-			end
-
-			if suit.key == "Diamonds" then
-				for i = 1, #G.deck.cards do
-					local other_card = G.deck.cards[i]
-					if other_card:is_suit("Diamonds") then
-						other_card.ability.perma_mult = other_card.ability.perma_mult or 0
-						other_card.ability.perma_mult = other_card.ability.perma_mult + card.ability.extra.upgrade
-						other_card.ability.perma_bonus = other_card.ability.perma_bonus or 0
-						other_card.ability.perma_bonus = other_card.ability.perma_bonus + card.ability.extra.upgrade
-						other_card:juice_up(0.5, 0.5)
-						SMODS.calculate_effect({ message = localize("k_upgrade_ex"), colour = G.C.CHIPS }, other_card)
-					end
-				end
-			end
-
-			if suit.key == "Spades" then
-				for i = 1, #G.deck.cards do
-					local other_card = G.deck.cards[i]
-					if other_card:is_suit("Spades") then
-						other_card.ability.perma_mult = other_card.ability.perma_mult or 0
-						other_card.ability.perma_mult = other_card.ability.perma_mult + card.ability.extra.upgrade
-						other_card.ability.perma_bonus = other_card.ability.perma_bonus or 0
-						other_card.ability.perma_bonus = other_card.ability.perma_bonus + card.ability.extra.upgrade
-						other_card:juice_up(0.5, 0.5)
-						SMODS.calculate_effect({ message = localize("k_upgrade_ex"), colour = G.C.CHIPS }, other_card)
-					end
+			for _, other_card in ipairs(G.deck.cards) do
+				if other_card:is_suit(suit.key) then
+					other_card.ability.perma_bonus = other_card.ability.perma_bonus or 0
+					other_card.ability.perma_bonus = other_card.ability.perma_bonus + card.ability.extra.upgrade
+					other_card.ability.perma_mult = other_card.ability.perma_mult or 0
+					other_card.ability.perma_mult = other_card.ability.perma_mult + card.ability.extra.upgrade
+					card_eval_status_text(other_card, "extra", nil, nil, nil, {
+						message = localize("k_upgrade_ex"),
+						colour = G.C.CHIPS,
+					})
 				end
 			end
 		end
