8f73befdf146c33a4057fe4316de67608a1f588e
blamperer <137224671+blamperer@users.noreply.github.com>
2025-08-29 02:42:05 -0400

    bring eggbug in line; G.playing_cards

--- modules/foods/eggbugegg.lua
+++ modules/foods/eggbugegg.lua
@@ -16,53 +16,15 @@ SMODS.Consumable({
 	end,
 	use = function(self, card, area, copier)
 		if card.ability.name == "eggbugegg" then
-			local suit = pseudorandom_element(SMODS.Suits, pseudoseed("mysuit"))
-
-			if suit.key == "Clubs" then
-				for i = 1, #G.deck.cards do
-					local other_card = G.deck.cards[i]
-					if other_card:is_suit("Clubs") then
-						other_card.ability.perma_bonus = other_card.ability.perma_bonus or 0
-						other_card.ability.perma_bonus = other_card.ability.perma_bonus + card.ability.extra.upgrade
-						other_card:juice_up(0.5, 0.5)
-						SMODS.calculate_effect({ message = localize("k_upgrade_ex"), colour = G.C.CHIPS }, other_card)
-					end
-				end
-			end
-
-			if suit.key == "Hearts" then
-				for i = 1, #G.deck.cards do
-					local other_card = G.deck.cards[i]
-					if other_card:is_suit("Hearts") then
-						other_card.ability.perma_bonus = other_card.ability.perma_bonus or 0
-						other_card.ability.perma_bonus = other_card.ability.perma_bonus + card.ability.extra.upgrade
-						other_card:juice_up(0.5, 0.5)
-						SMODS.calculate_effect({ message = localize("k_upgrade_ex"), colour = G.C.CHIPS }, other_card)
-					end
-				end
-			end
-
-			if suit.key == "Diamonds" then
-				for i = 1, #G.deck.cards do
-					local other_card = G.deck.cards[i]
-					if other_card:is_suit("Diamonds") then
-						other_card.ability.perma_bonus = other_card.ability.perma_bonus or 0
-						other_card.ability.perma_bonus = other_card.ability.perma_bonus + card.ability.extra.upgrade
-						other_card:juice_up(0.5, 0.5)
-						SMODS.calculate_effect({ message = localize("k_upgrade_ex"), colour = G.C.CHIPS }, other_card)
-					end
-				end
-			end
-
-			if suit.key == "Spades" then
-				for i = 1, #G.deck.cards do
-					local other_card = G.deck.cards[i]
-					if other_card:is_suit("Spades") then
-						other_card.ability.perma_bonus = other_card.ability.perma_bonus or 0
-						other_card.ability.perma_bonus = other_card.ability.perma_bonus + card.ability.extra.upgrade
-						other_card:juice_up(0.5, 0.5)
-						SMODS.calculate_effect({ message = localize("k_upgrade_ex"), colour = G.C.CHIPS }, other_card)
-					end
+			-- local suit = pseudorandom_element(SMODS.Suits, pseudoseed("mysuit"))
+			local suit = SCUG.get_suit_in_deck()
+			for i in 1, #G.playing_cards do
+				local other_card = G.playing_cards[i]
+				if other_card:is_suit(suit) then
+					other_card.ability.perma_bonus = other_card.ability.perma_bonus or 0
+					other_card.ability.perma_bonus = other_card.ability.perma_bonus + card.ability.extra.upgrade
+					other_card:juice_up(0.5, 0.5)
+					SMODS.calculate_effect({ message = localize("k_upgrade_ex"), colour = G.C.CHIPS }, other_card)
 				end
 			end
 		end
