e8fc783440d5f53470e7fdd6706c29e94c371c4e
blamperer <137224671+blamperer@users.noreply.github.com>
2025-12-13 01:24:38 -0500

    beehive: force two different suits

--- modules/weapons/beehive.lua
+++ modules/weapons/beehive.lua
@@ -20,8 +20,16 @@ SMODS.Sticker({
 		if context.setting_blind and G.GAME.blind.boss and context.main_eval then
 			--print ('a')
 			local suit = pseudorandom_element(SMODS.Suits, pseudoseed("mysuit"))
-			local suit_debuff = pseudorandom_element(SMODS.Suits, pseudoseed("ohshititsbees"))
-			
+			local suit_debuff = pseudorandom_element(
+				SMODS.Suits,
+				pseudoseed("ohshititsbees"),
+				{
+					in_pool = function(v)
+						return v.key ~= suit.key
+					end
+				}
+			)
+
 			for _, v in pairs(G.playing_cards) do
 				if v:is_suit(suit.name) then
 					if v.ability.perma_bonus <= 0 then
@@ -32,11 +40,11 @@ SMODS.Sticker({
 					end
 					v:juice_up(0.5, 0.5)
 					SMODS.calculate_effect({ message = localize("k_upgrade_ex"), colour = G.C.CHIPS }, v)
-				if v:is_suit(suit_debuff.name) then
+				elseif v:is_suit(suit_debuff.name) then
 					SMODS.debuff_card(v, true, "bees")
 				end
-				end
 			end
+
 			-- for i = 1, #G.deck.cards do
 			-- 	local chosen_cards = G.deck.cards[i]
 
