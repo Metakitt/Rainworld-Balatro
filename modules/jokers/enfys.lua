57691f3571411f85bf4e6bdf489fdcf50d0745c8
blamperer <137224671+blamperer@users.noreply.github.com>
2025-12-16 18:52:13 -0500

    enfys growth fix

--- modules/jokers/enfys.lua
+++ modules/jokers/enfys.lua
@@ -1,3 +1,14 @@
+local ENFYS_FOOD_LIST = {
+	"j_popcorn",
+	"j_gros_michel",
+	"j_ice_cream",
+	"j_egg",
+	"j_flower_pot",
+	"j_ramen",
+	"j_diet_cola",
+	"j_turtle_bean",
+}
+
 SMODS.Joker({
 	key = "enfys",
 	atlas = "slugcats",
@@ -36,7 +47,8 @@ SMODS.Joker({
 		end
 
 		if context.end_of_round and context.main_eval then
-			SMODS.add_card({ set = "Joker", area = G.jokers, edition = "e_negative", key = card.ability.extra.food })
+			local food, _ = pseudorandom_element(ENFYS_FOOD_LIST, "foods", {})
+			SMODS.add_card({ set = "Joker", area = G.jokers, edition = "e_negative", key = food })
 		end
 	end,
 })
