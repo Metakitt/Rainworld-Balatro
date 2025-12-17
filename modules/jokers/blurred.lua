2d9d7322310f9a42278c58a803b7f90878f1b739
blamperer <137224671+blamperer@users.noreply.github.com>
2025-12-17 01:22:50 -0500

    fix pup blur growth crash

--- modules/jokers/blurred.lua
+++ modules/jokers/blurred.lua
@@ -9,7 +9,8 @@ SMODS.Joker({
 	blueprint_compat = true,
 	config = { extra = { odds = 20 }, slugcat = true },
 	loc_vars = function(self, info_queue, card)
-		return { vars = { SMODS.get_probability_vars(card, 1, card.ability.extra.odds, "rw_blurred") }}
+		local numerator, denominator = SMODS.get_probability_vars(card, 1, card.ability.extra.odds, "rw_blurred")
+		return { vars = { numerator, denominator }}
 	end,
 	calculate = function(self, card, context)
 		if context.joker_main and not context.blueprint then
--- modules/jokers/pupblurred.lua
+++ modules/jokers/pupblurred.lua
@@ -7,24 +7,24 @@ SMODS.Joker({
 	unlocked = true,
 	discovered = true,
 	blueprint_compat = true,
-	config = { extra = { pupodds = 30, growth = 3}, slugcat = true },
+	config = { extra = { pupodds = 30, growth = 3 }, slugcat = true },
 	loc_vars = function(self, info_queue, card)
-	info_queue[#info_queue + 1] = { set = "Other", key = "slugpup_grows_up", vars = { card.ability.extra.growth } }
-		return { vars = { SMODS.get_probability_vars(card, 1, card.ability.extra.pupodds, "rw_blurred") }}
+		info_queue[#info_queue + 1] = { set = "Other", key = "slugpup_grows_up", vars = { card.ability.extra.growth } }
+		return { vars = { SMODS.get_probability_vars(card, 1, card.ability.extra.pupodds, "rw_blurred") } }
 	end,
 	calculate = function(self, card, context)
-	if context.setting_blind and not context.blueprint and card.ability.extra.growth > 0 then
-			card.ability.extra.growth = card.ability.extra.growth - 1
-			if card.ability.extra.growth <= 0 then
-				card.ability.extra.growth = nil
-				card:grow_up()
-			end
-		end
-
 		if context.setting_blind then
 			if SMODS.pseudorandom_probability(card, "rw_blurred", 1, card.ability.extra.pupodds, "rw_blurred") then
 				G.GAME.blind.chips = G.GAME.blind.chips / 2
 				G.GAME.blind.chip_text = number_format(G.GAME.blind.chips)
+			end
+		end
+
+		if context.setting_blind and not context.blueprint and card.ability.extra.growth > 0 then
+			card.ability.extra.growth = card.ability.extra.growth - 1
+			if card.ability.extra.growth <= 0 then
+				card.ability.extra.growth = nil
+				card:grow_up()
 			end
 		end
 	end,
