ada194aa3879bc82351974339f96ee4f7aeaaae8
blamperer <137224671+blamperer@users.noreply.github.com>
2026-09-24 13:10:57 -0400

    don't spawn enemies that kill you on boss blinds

--- modules/enemies.lua
+++ modules/enemies.lua
@@ -581,7 +581,7 @@ SCUG.spawn_enemy = function(args)
 
 				-- If this enemy exists, use it
 				if G.P_CENTERS[enemy_key] then
-					valid_selection = true
+					valid_selection = not (G.GAME.blind_on_deck == "Boss" and G.P_CENTERS[enemy_key]:has_attribute("killer"))
 					-- Otherwise, use one lower on the list (more likely to exist, probably)
 				else
 					sendWarnMessage("Enemy " .. enemy_key .. " not enabled!", "Rainworld")
